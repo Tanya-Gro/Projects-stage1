7a4f9bc51ec33ad155658dd27c12f34466d9db9e
unknown <tarpel.gta@gmail.com>
2025-01-12 23:49:33 +0200

    feat: add function generateTask

--- simon-says/gameScript.js
+++ simon-says/gameScript.js
@@ -5,6 +5,10 @@ const keyElements = document.querySelectorAll('.key');
 const numberElements = document.querySelectorAll('.keyboardContainer--numbers');
 const letterElements = document.querySelectorAll('.keyboardContainer--letter');
 
+let round = 1;
+let attempt = 1;
+let level = 1;
+
 for(let item of levelInputElements) {
   item.addEventListener('click', (e) => changeLevel(e))
 }
@@ -13,13 +17,16 @@ function changeLevel (event) {
   // console.log(event)
   getOffVisibility(keyElements);
   switch(event.target.value) {
-    case ('Easy'):
+    case (levelsArray[0]):
+      level = 1;
       getOnVisibility(numberElements);
       break;
-    case ('Medium'):
+    case (levelsArray[1]):
+      level = 2;
       getOnVisibility(letterElements);
       break;
-    case ('Hard'):
+    case (levelsArray[2]):
+      level = 3;
       getOnVisibility(numberElements);
       getOnVisibility(letterElements);
       break;
@@ -40,4 +47,23 @@ function getOnVisibility (NodeList) {
       item.classList.add('visible');
   }
 }
-// console.log(letters);
+
+function generateTask() {
+  let result = '';
+  let countSymbols = level === 1 ? numbers.length : level === 3 ? numbers.length + letters.length : letters.length;
+  let randomNum;
+  for(let i = 0; i < round * 2; i += 1) {
+    randomNum = Math.floor(Math.random() * countSymbols);
+    console.log(randomNum);
+    if(level === 1){
+      result += numbers[randomNum][0];
+    } else if(level === 2){
+      result += letters[randomNum][0];
+    } else {
+      if(randomNum < numbers.length) result += numbers[randomNum][0];
+      else result += letters[randomNum - numbers.length][0];
+    }
+  }
+  return result;
+}
+// console.log(generateTask());
